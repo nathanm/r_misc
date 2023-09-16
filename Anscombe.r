a392ac16ddd8ced8903ef5e62d225f2ce6555a27
Nathan Machula <nmachula@gmail.com>
2023-09-16 18:48:11 -0500

    Initial commit

---
+++ Anscombe.r
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(ggplot2)
+
+Anscombe <- anscombe %>%
+  pivot_longer(everything(),
+    names_to = c(".value", "set"),
+    names_pattern = "(.)(.)"
+  ) %>%
+  arrange(set)
+
+Anscombe %>%
+	group_by(set) %>%
+	summarize(
+		N = n(), 
+		mean.x = mean(x), 
+		sd.x = sd(x), 
+		mean.y = mean(y), 
+		sd.y = sd(y), 
+		corr.xy = cor(x, y)
+	)
+
+ggplot(data = Anscombe, aes(x = x, y = y)) +
+  geom_point() +
+  facet_wrap(~ set)
